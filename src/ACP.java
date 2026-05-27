d799f8faa17666f94979e2eb948b62b719dfb28f
LiahRiche <liahriche@gmail.com>
2026-05-27 15:35:38 +0200

    Update ACP.java

--- src/ACP.java
+++ src/ACP.java
@@ -1,53 +1,57 @@
-package facialRecognition;
+package projet;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import Jama.EigenvalueDecomposition;
+import javax.swing.*;
+import java.awt.*;
+
 import Jama.Matrix;
+import Jama.EigenvalueDecomposition;
 
-public class ACP {
+public class ACP extends JPanel{
 	private ArrayList <Vecteur> base = new ArrayList <Vecteur>();
 	private float seuil;
 	private int n;
 	private int m;
-	private Matrix matriceInitiale = new Matrix(n,m); //celle avec toutes les images
-	private Vecteur visageM = new Vecteur(n); //moyenne des pixels de toutes les images
-	private Matrix matriceEtude = new Matrix(n,m); //matriceInitiale à laquelle on a soustrait visageM à chacune des colonnes
-	private Matrix matriceReduite = new Matrix(m,m); //AT x A 
-	private Matrix matriceProjection;
+	private Matrix matriceInitiale = new Matrix(n,m);
+	private Vecteur visageM = new Vecteur(n);
+	private Matrix matriceEtude = new Matrix(n,m);
+	private Matrix matriceReduite = new Matrix(n,m);
+	private Matrix matriceProjection = new Matrix(n,m);
 	private Retour r = new Retour();
 	
-	public ACP(Matrix matriceInitiale) {
-		this.matriceInitiale = matriceInitiale; 
-		this.n = matriceInitiale.getRowDimension();
-		this.m = matriceInitiale.getColumnDimension();
-		//this.visageM = visageMoyen();
+	private double[] valeurs;
+	
+	public ACP(double[] valeurs) {
+		this.valeurs = valeurs;
+	}
+	
+	public ACP(Matrix matriceInitiale, int n, int m) {
+		this.matriceInitiale = matriceInitiale;
+		this.n=matriceInitiale.getRowDimension();
+		this.m=matriceInitiale.getColumnDimension();
 		visageMoyen();
 		soustraire();
 		multiplication();
-		valeursPropres(matriceReduite);
 		this.seuil = 80;
 	}
 	
 	
 	private void visageMoyen() {
-		//Vecteur v = new Vecteur(n);
-		for (int i=0; i<m; i++) {
+		for (int i=0; i<n; i++) {
 			int moyenne=0;
-			for (int j=0; j<n; j++) {
-				moyenne = (int) (moyenne + matriceInitiale.get(i,j));
+			for (int j=0; j<m; j++) {
+				moyenne =(int) (moyenne + matriceInitiale.get(i, j));
 			}
-			//v.setValueP(i, moyenne);
-			visageM.setValueP(i,moyenne);
+			visageM.setValueP(i, moyenne);
 		}
-		//return v;
 	}
 	
 	private void soustraire() {
 		for (int i=0; i<n; i++) {
 			for (int j=0; j<m; j++) {
-				matriceEtude.set(i, j, matriceInitiale.get(i,j) - visageM.getValue(i));
+				matriceEtude.set(i, j, matriceInitiale.get(i, j) - visageM.getValue(i));
 			}
 		}
 	}
@@ -61,22 +65,63 @@ public class ACP {
 //				for (int k=0; k<matriceEtude[0].size(); k++) {
 //					somme = somme + matriceEtude[i][k]*matriceTranspose[k][j];
 //				}
-//				m[i][j] = somme;
+//				matriceReduite[i][j] = somme;
 //			}
 //		}
 	}
 	
-	private static double[] valeursPropres(Matrix M) {
+	private static double[] valeurPropres(Matrix M) {
 		EigenvalueDecomposition eig = M.eig();
 		double[] valeurs = eig.getRealEigenvalues();
 		return valeurs;
 	}
 	
 	
+	@Override //redefini une méthode de la classe parent 
+	 protected void paintComponent(Graphics g) {
+
+	        super.paintComponent(g);
+
+	        // axes
+	        g.drawLine(50, 250, 350, 250); //axe horizontal (x), 
+	        //départ:(50, 250) arrivée: (350, 250), 50 pour ne pas coller 
+	        //au bord et 250 pour ne coller au sommet
+	        g.drawLine(50, 250, 50, 50); //axe vertical (y)
+
+	        // points
+	        for (int i = 0; i < valeurs.length; i++) {
+
+	            int x = 50 + i * 50;//points espacés de 50px
+
+	            int y = 250 - (int)(valeurs[i] * 20);//car java augmente les y vers le bas et on veut vers le haut 
+
+	            g.fillOval(x - 3, y - 3, 6, 6); //dessine un point (X, Y, largeur, hauteur), 
+	            //-3 car jave place à partir du coin en haut à gauche, on à un cercle de 6 de 
+	            //diamètre donc on enlève le rayon pour centré
+	        }
+	    }
+	
 	public static void main(String[] args) {
+		Matrix M = new Matrix(new double[][] {
+			{4, 2}, 
+			{1, 3}
+		});
+		System.out.println(Arrays.toString(valeurPropres(M)));
 		
-		Matrix M = new Matrix(2,2,1);
-		System.out.println(Arrays.toString(valeursPropres(M)));
-		
+		double[] vp = valeurPropres(M);
+
+        System.out.println(Arrays.toString(vp));
+
+        JFrame f = new JFrame("Valeurs propres"); //créer une fenêtre graphique appelée "Valeurs Propres"
+
+        ACP panneau = new ACP(vp); //Créer un panneau de dessin
+
+        f.add(panneau); //ajoute le panneau dans la fenêtre
+
+        f.setSize(400,300); //taille de la fenêtre
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //arrête le programme quand on ferme la fenêtre
+
+        f.setVisible(true); //affiche réellement la fenêtre
 	}
 }
