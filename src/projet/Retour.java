81eaa822127b9b1cf07fdf2c4e4d6d978928bff1
LiahRiche <liahriche@gmail.com>
2026-05-29 11:53:00 +0200

    Create Retour.java

---
+++ src/projet/Retour.java
@@ -0,0 +1,22 @@
+package projet;
+
+public class Retour {
+	private float pourcentage;
+	private Integer indice;
+	
+	public Retour() {}
+	
+	public float getPourcentage() {
+		return pourcentage;
+	}
+	public void setPourcentage(float pourcentage) {
+		this.pourcentage = pourcentage;
+	}
+	public Integer getIndice() {
+		return indice;
+	}
+	public void setIndice(Integer indice) {
+		this.indice = indice;
+	}
+	
+}
