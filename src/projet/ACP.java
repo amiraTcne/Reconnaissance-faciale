b2dda4a2c840da346911abbc976f8c626387a3a7
Amira <mimii.phoenix@gmail.com>
2026-06-01 01:30:42 +0200

    suppression de l'affichage du graphe dans la classe ACP + d'autres trucs

--- src/projet/ACP.java
+++ src/projet/ACP.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
 
 import java.lang.Math;
 
-import javax.swing.*;
-import java.awt.*;
-
 import Jama.Matrix;
 import Jama.EigenvalueDecomposition;
 
@@ -16,7 +13,7 @@ import Jama.EigenvalueDecomposition;
  * @author Liah, Amira
  * @version 1.0
  */
-public class ACP extends JPanel{
+public class ACP {
 	/** The basis of eigenvectors. */
 	private Vecteur[] base;
 	/** The threshold above which an image is considered to correspond to a person. */
@@ -57,7 +54,7 @@ public class ACP extends JPanel{
 		visageMoyen();
 		centrer();
 		reduireDimension();
-		creationBase();
+		creerBase();
 		this.seuil = (float) 0.8;
 	}
 	
@@ -91,30 +88,19 @@ public class ACP extends JPanel{
 		return eig.getRealEigenvalues();
 	}
 	
+	public double[] valeursProprestriees() {
+		double[] valeurs = valeursPropres();
+		//trie dans l'ordre croissant les vap
+		Arrays.sort(valeurs);
+		//inverse le tableau pour les avoir dans l'ordre décroissant
+		int n = valeurs.length;
+		double[] valeursTriees = new double[n];
+		for (int i = 0; i < n; i++) {
+            valeursTriees[i] = valeurs[n - 1 - i];
+        }
+		return valeursTriees;
+	}
 	
-	@Override //redefini une méthode de la classe parent 
-	  protected void paintComponent(Graphics g) { //utilise swing et awt pour créer et affiche le graphique
-
-	        super.paintComponent(g); //permet de partir d'un dessin vide
-
-	        // axes
-	        g.drawLine(50, 560, 5000, 560); //axe horizontal (x), 
-	        //départ:(50, 250) arrivée: (350, 250), 50 pour ne pas coller 
-	        //au bord et 250 pour ne coller au sommet
-	        g.drawLine(50, 1500, 50, 50); //axe vertical (y)
-
-	        // points
-	        for (int i = 0; i < valeurs.length; i++) {
-
-	            int x = 50 + i * 50;//points espacés de 50px
-
-	            int y = 560 - (int)(valeurs[i] * 20);//car java augmente les y vers le bas et on veut vers le haut 
-
-	            g.fillOval(x - 3, y - 3, 6, 6); //dessine un point (X, Y, largeur, hauteur), 
-	            //-3 car jave place à partir du coin en haut à gauche, on à un cercle de 6 de 
-	            //diamètre donc on enlève le rayon pour centré
-	        }
-	    }
 	
 	private void creerBase() {
 		EigenvalueDecomposition eig = matriceReduite.eig();
@@ -262,24 +248,27 @@ public class ACP extends JPanel{
 			{2, 2, 4, 7},
 			{4, 7, 1, 1}
 		});
-		System.out.println(Arrays.toString(valeursPropres(M)));
-		System.out.println(Arrays.toString(creationBase(M)));
+		ACP acp=new ACP(M);
+		double[] vap = acp.valeursPropres();
+		//Vecteur[] vep = acp.creerBase();
+		System.out.println(Arrays.toString(vap));
+		//System.out.println(Arrays.toString(creerBase()));
 		
-		double[] vp = valeursPropres(M);
+		
 
-        System.out.println(Arrays.toString(vp));
+        //System.out.println(Arrays.toString(vp));
 
         
-        JFrame f = new JFrame("Valeurs propres"); //créer une fenêtre graphique appelée "Valeurs Propres"
-
-        ACP panneau = new ACP(vp); //Créer un panneau de dessin
-
-        f.add(panneau); //ajoute le panneau dans la fenêtre
-
-        f.setSize(1000,700); //taille de la fenêtre
-
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //arrête le programme quand on ferme la fenêtre
-
-        f.setVisible(true); //affiche réellement la fenêtre
+//        JFrame f = new JFrame("Valeurs propres"); //créer une fenêtre graphique appelée "Valeurs Propres"
+//
+//        ACP panneau = new ACP(vp); //Créer un panneau de dessin
+//
+//        f.add(panneau); //ajoute le panneau dans la fenêtre
+//
+//        f.setSize(1000,700); //taille de la fenêtre
+//
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //arrête le programme quand on ferme la fenêtre
+//
+//        f.setVisible(true); //affiche réellement la fenêtre
 	}
 }
