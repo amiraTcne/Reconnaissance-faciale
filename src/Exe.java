fe79c394992a551e68f0c1e11c5e5aad58459989
LiahRiche <liahriche@gmail.com>
2026-06-02 16:52:01 +0200

    Update Exe.java

--- src/Exe.java
+++ src/Exe.java
@@ -9,13 +9,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import Jama.EigenvalueDecomposition;
+import Jama.Matrix;
+
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import java.awt.*;
 
 public class Exe{
     public static void main(String[] args){
-        Bdd test = new Bdd();
+        BDD test = new BDD();
         for(Personne p : test.imagesBdd.keySet()) {
             System.out.println(p.getPrenom());
 			for(ImageVisage img : test.imagesBdd.get(p)) {
@@ -35,55 +38,16 @@ public class Exe{
         /**Test ACP*/
         
         /**Affichage du visage moyen*/
-        Bdd bdd = new Bdd(); 
+        BDD bdd = new BDD(); 
         ACP acp = new ACP(bdd.createA());
+        DisplayImg d = new DisplayImg();
 
         Vecteur vecteur = acp.getVisageM();
         int largeur = 100;
         int hauteur = 100;
-
-        BufferedImage img =
-                new BufferedImage(
-                        largeur,
-                        hauteur,
-                        BufferedImage.TYPE_BYTE_GRAY);
-
-        for(int y=0;y<hauteur;y++){
-
-            for(int x=0;x<largeur;x++){
-
-                int indice =
-                        x * hauteur + y;
-
-                int valeur =
-                        (int)vecteur.getValue(
-                                indice);
-
-                valeur =
-                        Math.max(
-                                0,
-                                Math.min(
-                                        255,
-                                        valeur));
-
-                img.getRaster().setSample(
-                        x,
-                        y,
-                        0,
-                        valeur);
-            }
-        }
-        JFrame frame = new JFrame();
-
-        frame.pack();
         
-        frame.setSize(500,500);
-
-        frame.add(new JLabel(new ImageIcon(img)));
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.setVisible(true);
+        d.affImg(vecteur);
+        
         
         /**Affichage du graphe des valeurs propres*/	    
 	    GrapheValeursPropres graphe = new GrapheValeursPropres(acp);
@@ -94,5 +58,22 @@ public class Exe{
 	    f.setSize(2000, 1000); //taille de la fenêtre
 	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //arrête le programme quand on ferme la fenêtre
 	    f.setVisible(true); //affiche réellement la fenêtre
+	    
+	    /**Affichage valeur propre (image)*/
+	    vecteur = acp.eigenfaces()[5];
+	    d.affImg(vecteur);
+        
+        /**Comparaison*/
+        ImageVisage im = new ImageVisage(101, "dataset/reference/Zendaya_Coleman_3.jpg");
+        d.affImg(im.process());
+        Retour[] tab = acp.tableauComparaison(im);
+        System.out.println(tab[0].getPourcentage());
+        System.out.println(tab[99].getPourcentage());
+        
+        /**Affichage de l'image la plus proche*/
+        vecteur = acp.prendreVecteur(tab[0].getIndice(), acp.getMatriceInitiale());
+        d.affImg(vecteur);
+        vecteur = acp.prendreVecteur(tab[99].getIndice(), acp.getMatriceInitiale());
+        d.affImg(vecteur);
     }
 }
