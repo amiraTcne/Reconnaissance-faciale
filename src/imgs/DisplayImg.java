d8c6e6f8d6d083eee21674af1494345a731ac365
sorcero <sorcero@tutanota.com>
2026-06-02 15:06:50 +0200

    comments in DisplayImg and minor additions

--- src/imgs/DisplayImg.java
+++ src/imgs/DisplayImg.java
@@ -11,7 +11,18 @@ import java.io.IOException;
 
 import Jama.Matrix;
 
+/**
+ * The purpose of this class is to provide static methods to display images.
+ * @author Pernet Gabriel
+ * @version 1.0
+ */
 public class DisplayImg{
+    /**
+     * This function takes the vectorial representation of a grayscale image and displays it.
+     * @param imgVect the <code>Vecteur</code> representing the image
+     * @param h number of rows of the <code>Matrix</code> representing the image
+     * @param w number of columns of the <code>Matrix</code> representing the image
+     */
     public static void affImg(Vecteur imgVect,int h,int w){
         int indice;
         int valeur;
@@ -30,21 +41,41 @@ public class DisplayImg{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+    /**
+     * This function takes the vectorial representation of a grayscale image of dimentions 100x100 and displays it.
+     * This can be used on the processed images.
+     * @param imgVect the <code>Vecteur</code> representing the image
+     */
+    public static void affImg(Vecteur imgVect){
+        affImg(imgVect,100,100);
+    }
+    /**
+     * This function takes the matrix representation of a grayscale image and displays it.
+     * @param imgMat the <code>Matrix</code> representing the image
+     */
     public static void affImg(Matrix imgMat){
         Vecteur imgVect = new Vecteur(imgMat);
         affImg(imgVect,imgMat.getRowDimension(),imgMat.getColumnDimension());
     }
+    /**
+     * This function takes an <code>ImageVisage</code> and displays the processed version of the image linked to it.
+     * @param img the <code>ImageVisage</code> linked to the image to display in its processed form
+     */
     public static void affImgP(ImageVisage img){
         Matrix imgMat = img.processed();
         affImg(imgMat);
     }
+    /**
+     * This function takes an <code>ImageVisage</code> and displays the image linked to it.
+     * @param img the <code>ImageVisage</code> linked to the image to display
+     */
     public static void affImgO(ImageVisage img){
         try{
             File imgFile = new File(img.getPath());
             BufferedImage bimg = ImageIO.read(imgFile);
             JFrame frame = new JFrame();
             frame.pack();
-            frame.setSize(500,500);
+            frame.setSize(750,750);
             frame.add(new JLabel(new ImageIcon(bimg)));
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
