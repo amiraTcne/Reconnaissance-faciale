f65d2107155a5ba65676e13bca30106493fdab73
Syliamedd <syliamd88@gmail.com>
2026-06-10 18:34:59 +0200

    Update VueCorrespondanceTrouvee.java

--- src/IHM/VueCorrespondanceTrouvee.java
+++ src/IHM/VueCorrespondanceTrouvee.java
@@ -1,6 +1,9 @@
 package IHM;
 
 import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -13,13 +16,15 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 import laBdd.Bdd;
 import laBdd.Personne;
-import calculs.ACP;
-import calculs.Retour;
+import projet.ACP;
+import projet.Retour;
 import imgs.ImageVisage;
 public class VueCorrespondanceTrouvee extends VBox{
     Bdd bdd = new Bdd(); 
     ACP acp = new ACP(bdd.createA());
-	public VueCorrespondanceTrouvee(File imageSelectionnee,Retour[] tableauPourcentage) {
+    private CtrlImport ctrlImport;
+	public VueCorrespondanceTrouvee(File imageSelectionnee,Retour[] tableauPourcentage, CtrlImport ctrlImport) {
+		this.ctrlImport= ctrlImport;
 		this.setSpacing(20);
         this.setAlignment(Pos.CENTER);
         HBox troisColonnes = creerLaVue(imageSelectionnee, tableauPourcentage);
@@ -37,34 +42,48 @@ public class VueCorrespondanceTrouvee extends VBox{
         // Left : selected image + "no match" message
         ImageView preview = new ImageView(new Image(imageSelectionnee.toURI().toString()));
         ImageVisage nouvelleIm = new ImageVisage(0, imageSelectionnee.getPath());
-        preview.setFitWidth(220);
-        preview.setFitHeight(220);
+        preview.setFitWidth(250);
+        preview.setFitHeight(250);
         preview.setPreserveRatio(true);
         //bloc gauche sous-titres
         Label legende = new Label("Image sélectionnée");
         legende.setStyle("-fx-font-style: italic; -fx-text-fill: gray;");
         Label message = new Label("✅ Correspondance trouvée !");
-        message.setStyle("-fx-font-size: 14px; -fx-font-weight: bold; -fx-text-fill: #2E7D32;");
+        message.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-text-fill: #2E7D32;");
         message.setWrapText(true);
         message.setTextAlignment(TextAlignment.CENTER);
-        message.setMaxWidth(220);
-        VBox gauche = new VBox(15, preview, legende,message);
+        message.setMaxWidth(250);
+        
+        VBox gauche = new VBox(15, preview, legende, message);
         gauche.setAlignment(Pos.CENTER);
-        //bloc centre 
-        Label labelComparaison = new Label("👤 ⟷ 👤");
-        labelComparaison.setStyle("-fx-font-size: 36px; -fx-text-fill: black;");
-        Label lblTitre = new Label("Correspondance ");
-        lblTitre.setStyle("-fx-font-size: 22px; -fx-font-weight: bold; -fx-text-fill: #2E7D32;");
-        VBox zoneImport = new VBox(10, labelComparaison, lblTitre);
+        gauche.setPadding(new Insets(0, 0, 80, 0));
+        
+        // Center : re-import zone
+        Label labelImport = new Label("📁  Importer une nouvelle photo");
+        labelImport.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
+
+        Label placeholder = new Label("Sélectionnez la photo que vous souhaitez analyser");
+        placeholder.setStyle("-fx-text-fill: gray;");
+
+        Button boutonImporter = new Button("+");
+        boutonImporter.setStyle(
+                "-fx-background-color: #43A047; -fx-text-fill: white; " +
+                        "-fx-font-size: 18px; -fx-min-width: 40; -fx-min-height: 40; -fx-background-radius: 4;"
+        );
+        boutonImporter.setOnAction(ctrlImport);
+
+        VBox zoneImport = new VBox(10, labelImport, placeholder, boutonImporter);
         zoneImport.setAlignment(Pos.CENTER);
         zoneImport.setPadding(new Insets(30));
         zoneImport.setStyle("-fx-border-color: #ddd; -fx-border-radius: 8; -fx-background-color: white;");
+
         VBox centre = new VBox(zoneImport);
         centre.setAlignment(Pos.CENTER);
-     // --- Right : nav buttons + large display + thumbnail strip (15 closest) ---
+        centre.setPadding(new Insets(0, 0, 60, 110));
+        // Right : nav buttons + large display + thumbnail strip
         ImageView imageComparee = new ImageView();
-        imageComparee.setFitWidth(180);
-        imageComparee.setFitHeight(180);
+        imageComparee.setFitWidth(250);
+        imageComparee.setFitHeight(250);
         imageComparee.setPreserveRatio(true);
 
         Label nomPersonne = new Label();
@@ -73,7 +92,7 @@ public class VueCorrespondanceTrouvee extends VBox{
         Label pourcentage = new Label();
         pourcentage.setStyle("-fx-text-fill: gray;");
         
-        HBox bandeAutresImages = new HBox(15);
+        HBox bandeAutresImages = new HBox(50);
         bandeAutresImages.setAlignment(Pos.CENTER);
 
         Button precedent = new Button("Précédent");
@@ -109,8 +128,8 @@ public class VueCorrespondanceTrouvee extends VBox{
             for (ImageVisage autreImg : bdd.imagesBdd.get(p)) {
                 if (autreImg.id != img.id) {
                     ImageView mini = new ImageView(new Image(new File(autreImg.getPath()).toURI().toString()));
-                    mini.setFitWidth(65);
-                    mini.setFitHeight(65);
+                    mini.setFitWidth(100);
+                    mini.setFitHeight(100);
                     mini.setPreserveRatio(true);
 
                     float pMini = acp.pourcentageImage(nouvelleIm, autreImg.id);
@@ -142,8 +161,8 @@ public class VueCorrespondanceTrouvee extends VBox{
             Retour r = tableauPourcentage[i];
             ImageVisage img = bdd.getImg(r.getIndice());
             ImageView icone = new ImageView(new Image(new File(img.getPath()).toURI().toString()));
-            icone.setFitWidth(60);
-            icone.setFitHeight(60);
+            icone.setFitWidth(150);
+            icone.setFitHeight(150);
             icone.setPreserveRatio(true);
 
             Button bouton = new Button();
@@ -157,18 +176,18 @@ public class VueCorrespondanceTrouvee extends VBox{
 
         ScrollPane scrollVignettes = new ScrollPane(colonneVignettes);
         scrollVignettes.setFitToWidth(true);
-        scrollVignettes.setPrefWidth(90);
+        scrollVignettes.setPrefWidth(300);
         scrollVignettes.setPrefViewportHeight(300);
 
         // select the closest match by default
         maj.run();
 
-        HBox droite = new HBox(15, grandAffichage, scrollVignettes);
+        HBox droite = new HBox(100, grandAffichage, scrollVignettes);
         droite.setAlignment(Pos.CENTER);
 
         // Assembly
         HBox vue = new HBox(40, gauche, centre, droite);
-        vue.setAlignment(Pos.CENTER);
+        vue.setAlignment(Pos.TOP_CENTER);
         vue.setPadding(new Insets(30));
         return vue;
     }
